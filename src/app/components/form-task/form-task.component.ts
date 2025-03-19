d2d3f2b86cb260b0467aa189e68cc760f45df848
Sarkande <aperezgo74@gmail.com>
2025-03-19 12:35:23 +0100

    Localizes form labels and error messages to French
    Updates placeholder text, labels, and error messages in user project and task forms to French.
    Adds star rating component for task priority and improves validation.
    Enhances form layout by adding margin and disabling submit button when form is invalid.

--- src/app/components/form-task/form-task.component.ts
+++ src/app/components/form-task/form-task.component.ts
@@ -1,15 +1,32 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, Input } from '@angular/core';
+import {
+    FormGroup,
+    FormControl,
+    Validators,
+    FormsModule,
+    ReactiveFormsModule,
+} from '@angular/forms';
+import { StarRatingComponent } from '../star-rating/star-rating.component';
 
 @Component({
     selector: 'app-form-task',
     standalone: true,
-    imports: [CommonModule, FormsModule, ReactiveFormsModule],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        StarRatingComponent,
+    ],
     templateUrl: './form-task.component.html',
     styleUrls: ['./form-task.component.scss'],
 })
 export class FormTaskComponent {
+    //Getter pour eviter les problemes de compilation avec un priorité null
+    get priorityValue(): number {
+        return this.taskForm.get('priority')?.value ?? 1;
+    }
+
     taskForm = new FormGroup({
         name: new FormControl('', [
             Validators.required,
@@ -25,6 +42,7 @@ export class FormTaskComponent {
         status: new FormControl('pending'),
     });
 
+    
     onSubmit() {
         if (this.taskForm.valid) {
             console.log('Form Submitted!', this.taskForm.value);
---
+++ src/app/components/star-rating/star-rating.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+    selector: 'app-star-rating',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './star-rating.component.html',
+    styleUrls: ['./star-rating.component.scss'],
+})
+export class StarRatingComponent {
+    @Input() priority: number = 1;
+    @Output() priorityChange = new EventEmitter<number>();
+    stars: number[] = [1, 2, 3, 4, 5];
+    hoveredIndex: number = -1;
+
+    setPriority(newPriority: number): void {
+        this.priority = newPriority + 1;
+        if (this.priority <= 1) {
+            this.priority = 1;
+        }
+        if (this.priority >= this.stars.length) {
+            this.priority = this.stars.length;
+        }
+
+        this.priorityChange.emit(this.priority);
+    }
+
+    setHover(index: number): void {
+        this.hoveredIndex = index;
+    }
+
+    clearHover(): void {
+        this.hoveredIndex = -1;
+    }
+}
